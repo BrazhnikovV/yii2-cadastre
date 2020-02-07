9dc1263fa07f11116c6edaab997a20bae1e47c27
BrazhnikovV <brazhnikov_ve@mail.ru>
2020-02-07 11:05:41 +0300

    Добавил папку assets для ресурсов.

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -10,7 +10,7 @@ class MainController extends Controller
     public $layout = 'main';
     public function actionIndex() {
         // регистрируем ресурсы:
-        //\brazhnikov\yii2cadastre\AppAssetsBundle::register($this->view);
+        \brazhnikov\yii2cadastre\AppAssetsBundle::register($this->view);
 
         return $this->render('index',[
             'datas' => 'asasasasas'
